e29e1a4607ff901e90e731be0f21bbebed1415a1
KZ <kjreem.imagine@gmail.com>
2022-12-30 21:52:38 +0330

    fixed components

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -21,32 +21,26 @@ export default function Body() {
 
   let component;
 
-  // if (!token) {
-  //     switch (path) {
-  //   default:
-  //     component = <LandingPage />;
-  //     break;
-  //   case "home":
-  //     component = <LandingPage />;
-  //     break;
-  //   case "news":
-  //     component = <NewsPage />;
-  //     break;
-  //   case "contracts":
-  //     component = <Contracts />;
-  //     break;
-  //   case "investment":
-  //     component = <Investment />;
-  //     break;
-  //   case "about":
-  //     component = <AboutUs />;
-  //     break;
-  // }
-  // } else {
-  //   component = <AdminPanel />
-  // }
-
-
+  switch (path) {
+    default:
+      component = <LandingPage />;
+      break;
+    case "home":
+      component = <LandingPage />;
+      break;
+    case "news":
+      component = <NewsPage />;
+      break;
+    case "contracts":
+      component = <Contracts />;
+      break;
+    case "investment":
+      component = <Investment />;
+      break;
+    case "about":
+      component = <AboutUs />;
+      break;
+  }
 
   return (
     <>

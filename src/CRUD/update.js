f712f666daa4b8611ec5c633609033ecc8ff9f10
RammeDon <raminkhareji@gmail.com>
2023-01-03 20:16:02 +0100

    func

--- src/CRUD/update.js
+++ src/CRUD/update.js
@@ -31,23 +31,49 @@ const updateUser = async (updateDetails, id) => {
         });
 }
 
-const updateContract = async (updateDetails, id) => {
-    const contractDetails = {
-        title: updateDetails.title,
-        userID: updateDetails.userID,
+const updateAccount = async (updateDetails, id) => {
+    const accountDetails = {
+        bankNames: updateDetails.bankNames,
+        accountNumbers: updateDetails.accountNumbers,
         username: updateDetails.username,
-        firstname: updateDetails.firstname,
-        lastname: updateDetails.lastname,
-        birthDay: updateDetails.birthDay,
-        phone: updateDetails.phone,
-        email: updateDetails.email,
+        swiftCode: updateDetails.swiftCode,
         country: updateDetails.country,
-        language: updateDetails.language,
-        accoutID: updateDetails.accountID,
-        contractID: updateDetails.contractID,
-        personalSettings: updateDetails.personalSettings,
-        profilePictureID: updateDetails.profilePictureID
+        currency: updateDetails.currency,
+        balance: updateDetails.balance,
+        depositeAccount: updateDetails.depositeAccount,
+        paymentHistory: updateDetails.paymentHistory,
+        contractType: updateDetails.contractType,
+        todayTrades: updateDetails.todayTrades,
+        newTrades: updateDetails.newTrades,
+        todayMoney: updateDetails.todayMoney,
+        profilePictureID: updateDetails.profilePictureID,
+        days: updateDetails.days,
+        percentageInTrades: updateDetails.percentageInTrades,
+        balanceOverView: updateDetails.balanceOverView,
+        activeTradesToday: updateDetails.activeTradesToday,
+        activeTradesLastWeek: updateDetails.activeTradesLastWeek
     };
+
+    fetch(`/api/accounts/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(accountDetails)
+    })
+        .then(res => res.json())
+        .then(response => {
+            if (response.message) {
+                console.log(response.message);
+            }
+        });
 }
 
-export default {updateUser, updateContract}
+
+const exports = {
+    updateUser,
+    updateAccount,
+  };
+  
+  export default exports;
+  

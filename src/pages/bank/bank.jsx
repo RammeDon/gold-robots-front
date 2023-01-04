791214e1bc0c117b134c0a7f3f18770d907ca54a
RammeDon <raminkhareji@gmail.com>
2023-01-04 14:40:01 +0100

    git

--- src/pages/bank/bank.jsx
+++ src/pages/bank/bank.jsx
@@ -519,7 +519,7 @@ function Action(props) {
     username: props.user.username,
     paymentSystem: "",
     ammount: "0",
-    paymentType: "Withdraw",
+    paymentType: props.action === 0 ? "Deposit" : "Withdraw",
   });
 
   const ButtonTheme = createTheme({

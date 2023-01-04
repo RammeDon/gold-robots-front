4888bd488d4b7f5757ac9c11eb329159747f23af
RammeDon <raminkhareji@gmail.com>
2023-01-04 18:07:08 +0100

    package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {React, BrowserRouter} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <BrowserRouter basename={basename} />
   </React.StrictMode>
   
 );

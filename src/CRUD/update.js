2ffa6d15a12c6e2b6f1535ac7fb6d65630abc23e
KZ <kjreem.imagine@gmail.com>
2023-01-04 14:31:45 +0330

    contract fix 3

--- src/CRUD/update.js
+++ src/CRUD/update.js
@@ -1,10 +1,9 @@
 import { env } from "../environment/environment";
 
-
 const updateUser = async (updateDetails, id) => {
-    const userDetails = {
-        profilePictureID: updateDetails
-    };
+  const userDetails = {
+    profilePictureID: updateDetails,
+  };
 
   fetch(`${env.baseUrl}/api/users/${id}`, {
     method: "PUT",
@@ -22,49 +21,53 @@ const updateUser = async (updateDetails, id) => {
 };
 
 const updateAccount = async (updateDetails, id) => {
-    console.log("details", updateDetails)
-    const accountDetails = {
-        bankNames: updateDetails.bankNames,
-        accountNumbers: updateDetails.accountNumbers,
-        swiftCode: updateDetails.swiftCode,
-        country: updateDetails.country,
-        currency: updateDetails.currency,
-    };
-
-    fetch(`${env.baseUrl}/api/accounts/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(accountDetails)
-    })
-        .then(res => res.json())
-        .then(response => {
-            if (response.message) {
-                console.log(response.message);
-            }
-        });
-}
-
-const updateContract = async (updateDetails, id) => {
-  const contractDetails = {
-    title: updateDetails.title,
-    userID: updateDetails.userID,
-    username: updateDetails.username,
-    firstname: updateDetails.firstname,
-    lastname: updateDetails.lastname,
-    birthDay: updateDetails.birthDay,
-    phone: updateDetails.phone,
-    email: updateDetails.email,
+  console.log("details", updateDetails);
+  const accountDetails = {
+    bankNames: updateDetails.bankNames,
+    accountNumbers: updateDetails.accountNumbers,
+    swiftCode: updateDetails.swiftCode,
     country: updateDetails.country,
-    language: updateDetails.language,
-    accoutID: updateDetails.accountID,
-    contractID: updateDetails.contractID,
-    personalSettings: updateDetails.personalSettings,
-    profilePictureID: updateDetails.profilePictureID,
+    currency: updateDetails.currency,
   };
 
-  fetch(`${env.baseUrl}/api/contracts/${id}`, {
+  fetch(`${env.baseUrl}/api/accounts/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(accountDetails),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      if (response.message) {
+        console.log(response.message);
+      }
+    });
+};
+
+const updateContract = async (updateDetails, username, contractname) => {
+  const contractDetails = {
+    username: updateDetails.username,
+    ammount: updateDetails.ammount,
+    color: updateDetails.color,
+    date: updateDetails.date,
+    garantiePrecent: updateDetails.garantiePrecent,
+    level: updateDetails.level,
+    maxTradeDays: updateDetails.maxTradeDays,
+    minProfit: updateDetails.minProfit,
+    name: updateDetails.name,
+    minDuration: updateDetails.minDuration,
+    minDeposite: updateDetails.profitSharing,
+    adminClients: updateDetails.adminClients,
+    profitSharing: updateDetails.profitSharing,
+    extraDays: updateDetails.extraDays,
+    timeframe: updateDetails.timeframe,
+    leverage: updateDetails.leverage,
+    timerange: updateDetails.timerange,
+    maxUsage: updateDetails.maxUsage,
+  };
+
+  fetch(`${env.baseUrl}/api/contracts/${username}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
--- src/pages/personal-settings/presonal-settings.jsx
+++ src/pages/personal-settings/presonal-settings.jsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Button,
   Card,
   FormControl,
@@ -6,8 +7,10 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Snackbar,
 } from "@mui/material";
 import { useState } from "react";
+import update from "../../CRUD/update";
 import create from "../../CRUD/create";
 
 export function PersonalSetting(props) {
@@ -19,201 +22,216 @@ export function PersonalSetting(props) {
     extraDays: "",
     maxUsage: "",
   });
+  const [openAlert, setOpenAlert] = useState(false);
 
   const handelSubmission = async () => {
     const newContract = { ...selectedContract, ...settings };
 
-
-    await create.createContract(newContract).then(() => {
-      console.log(newContract);
-    });
+    if (props.contract.length < 2) {
+      await create.createContract(newContract).then(() => {
+        console.log(newContract);
+      });
+    } else {
+      setOpenAlert(true);
+    }
   };
 
   return (
-    <Grid container sx={{ justifyContent: "start", gap: "12px" }}>
-      <Grid md={6} sm={12}>
-        <FormControl>
-          <InputLabel id="contract" sx={{ color: "white" }}>
-            Chosen contract
-          </InputLabel>
-          <Select
-            sx={{ width: "300px" }}
-            inputProps={{ sx: { color: "white" } }}
-            labelId="contract"
-            label="Chosen contract"
-            value={selectedContract}
-            onChange={(e) => {
-              setSelectedContract(props.contracts[e.target.value]);
-            }}
-          >
-            {props.contracts.map((contract, i) => {
-              return (
-                <MenuItem value={i} key={contract._id} sx={{}}>
-                  {contract.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </Grid>
-      <Grid md={12} sm={12}>
-        <Card
-          sx={{
-            backgroundColor: "#111827",
-            width: "80%",
-            minHeight: "100px",
-            mx: "auto",
-          }}
-        >
-          <Grid
-            container
+    <>
+      <Snackbar
+        autoHideDuration={4000}
+        onClose={() => {
+          setOpenAlert(false);
+        }}
+        open={openAlert}
+      >
+        <Alert severity="error">cannot add more then two contracts</Alert>
+      </Snackbar>
+      <Grid container sx={{ justifyContent: "start", gap: "12px" }}>
+        <Grid md={6} sm={12}>
+          <FormControl>
+            <InputLabel id="contract" sx={{ color: "white" }}>
+              Chosen contract
+            </InputLabel>
+            <Select
+              sx={{ width: "300px" }}
+              inputProps={{ sx: { color: "white" } }}
+              labelId="contract"
+              label="Chosen contract"
+              value={selectedContract}
+              onChange={(e) => {
+                setSelectedContract(props.contracts[e.target.value]);
+              }}
+            >
+              {props.contracts.map((contract, i) => {
+                return (
+                  <MenuItem value={i} key={contract._id} sx={{}}>
+                    {contract.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid md={12} sm={12}>
+          <Card
             sx={{
-              gap: "30px",
-              width: "100%",
-              height: "100",
-              justifyContent: "center",
-              p: 3,
+              backgroundColor: "#111827",
+              width: "80%",
+              minHeight: "100px",
+              mx: "auto",
             }}
           >
-            <Grid md={5} sm={12}>
-              <FormControl>
-                <InputLabel sx={{ color: "white" }} id="leverage">
-                  Leverage
-                </InputLabel>
-                <Select
-                  inputProps={{ sx: { color: "white" } }}
-                  sx={{ width: 300 }}
-                  label="Leverage"
-                  labelId="leverage"
-                  value={settings.leverage}
-                  onChange={(e) => {
-                    setSettings((current) => ({
-                      ...current,
-                      leverage: e.target.value,
-                    }));
-                  }}
+            <Grid
+              container
+              sx={{
+                gap: "30px",
+                width: "100%",
+                height: "100",
+                justifyContent: "center",
+                p: 3,
+              }}
+            >
+              <Grid md={5} sm={12}>
+                <FormControl>
+                  <InputLabel sx={{ color: "white" }} id="leverage">
+                    Leverage
+                  </InputLabel>
+                  <Select
+                    inputProps={{ sx: { color: "white" } }}
+                    sx={{ width: 300 }}
+                    label="Leverage"
+                    labelId="leverage"
+                    value={settings.leverage}
+                    onChange={(e) => {
+                      setSettings((current) => ({
+                        ...current,
+                        leverage: e.target.value,
+                      }));
+                    }}
+                  >
+                    <MenuItem value={"1:100"}>1:100</MenuItem>
+                    <MenuItem value={"1:200"}>1:200</MenuItem>
+                    <MenuItem value={"1:300"}>1:500</MenuItem>
+                    <MenuItem value={"1:400"}>1:1000</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid md={5} sm={12}>
+                <FormControl>
+                  <InputLabel sx={{ color: "white" }} id="days">
+                    Add Extra Day
+                  </InputLabel>
+                  <Select
+                    inputProps={{ sx: { color: "white" } }}
+                    sx={{ width: 300 }}
+                    label="Add Extra Day"
+                    labelId="days"
+                    value={settings.extraDays}
+                    onChange={(e) => {
+                      setSettings((current) => ({
+                        ...current,
+                        extraDays: e.target.value,
+                      }));
+                    }}
+                  >
+                    <MenuItem value={"5 Days"}>5 Days</MenuItem>
+                    <MenuItem value={"10 Days"}>10 Days</MenuItem>
+                    <MenuItem value={"25 Days"}>25 Days</MenuItem>
+                    <MenuItem value={"50 Days"}>50 Days</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid md={5} sm={12}>
+                <FormControl>
+                  <InputLabel sx={{ color: "white" }} id="timeframe">
+                    Trading timeframe
+                  </InputLabel>
+                  <Select
+                    sx={{ width: 300 }}
+                    label="Trading timeframe"
+                    labelId="timeframe"
+                    inputProps={{ sx: { color: "white" } }}
+                    value={settings.timeframe}
+                    onChange={(e) => {
+                      setSettings((current) => ({
+                        ...current,
+                        timeframe: e.target.value,
+                      }));
+                    }}
+                  >
+                    <MenuItem value={"1 min"}>1 min</MenuItem>
+                    <MenuItem value={"5 min"}>5 min</MenuItem>
+                    <MenuItem value={"15 min"}>15 min</MenuItem>
+                    <MenuItem value={"1 hour"}>1 hour</MenuItem>
+                    <MenuItem value={"1 day"}>1 day</MenuItem>
+                    <MenuItem value={"all"}>All</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid md={5} sm={12}>
+                <FormControl>
+                  <InputLabel sx={{ color: "white" }} id="timerange">
+                    Time range
+                  </InputLabel>
+                  <Select
+                    sx={{ width: 300 }}
+                    label="Time range"
+                    labelId="timerange"
+                    inputProps={{ sx: { color: "white" } }}
+                    value={settings.timerange}
+                    onChange={(e) => {
+                      setSettings((current) => ({
+                        ...current,
+                        timerange: e.target.value,
+                      }));
+                    }}
+                  >
+                    <MenuItem value={0}>London time</MenuItem>
+                    <MenuItem value={1}>Tokyo time</MenuItem>
+                    <MenuItem value={2}>New york time</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid md={5} sm={12}>
+                <FormControl>
+                  <InputLabel sx={{ color: "white" }} id="MUD">
+                    Max usage deposite
+                  </InputLabel>
+                  <Select
+                    sx={{ width: 300 }}
+                    label="Max usage deposite"
+                    labelId="MUD"
+                    inputProps={{ sx: { color: "white" } }}
+                    value={settings.maxUsage}
+                    onChange={(e) => {
+                      setSettings((current) => ({
+                        ...current,
+                        maxUsage: e.target.value,
+                      }));
+                    }}
+                  >
+                    <MenuItem value={"35 %"}>35 %</MenuItem>
+                    <MenuItem value={"50 %"}>50 %</MenuItem>
+                    <MenuItem value={"85 %"}>85 %</MenuItem>
+                    <MenuItem value={"100 %"}>100 %</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid md={5} sm={12}>
+                <Button
+                  sx={{ color: "white", width: 200, height: 60 }}
+                  color="secondary"
+                  variant="contained"
+                  onClick={handelSubmission}
                 >
-                  <MenuItem value={"1:100"}>1:100</MenuItem>
-                  <MenuItem value={"1:200"}>1:200</MenuItem>
-                  <MenuItem value={"1:300"}>1:500</MenuItem>
-                  <MenuItem value={"1:400"}>1:1000</MenuItem>
-                </Select>
-              </FormControl>
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
-            <Grid md={5} sm={12}>
-              <FormControl>
-                <InputLabel sx={{ color: "white" }} id="days">
-                  Add Extra Day
-                </InputLabel>
-                <Select
-                  inputProps={{ sx: { color: "white" } }}
-                  sx={{ width: 300 }}
-                  label="Add Extra Day"
-                  labelId="days"
-                  value={settings.extraDays}
-                  onChange={(e) => {
-                    setSettings((current) => ({
-                      ...current,
-                      extraDays: e.target.value,
-                    }));
-                  }}
-                >
-                  <MenuItem value={"5 Days"}>5 Days</MenuItem>
-                  <MenuItem value={"10 Days"}>10 Days</MenuItem>
-                  <MenuItem value={"25 Days"}>25 Days</MenuItem>
-                  <MenuItem value={"50 Days"}>50 Days</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid md={5} sm={12}>
-              <FormControl>
-                <InputLabel sx={{ color: "white" }} id="timeframe">
-                  Trading timeframe
-                </InputLabel>
-                <Select
-                  sx={{ width: 300 }}
-                  label="Trading timeframe"
-                  labelId="timeframe"
-                  inputProps={{ sx: { color: "white" } }}
-                  value={settings.timeframe}
-                  onChange={(e) => {
-                    setSettings((current) => ({
-                      ...current,
-                      timeframe: e.target.value,
-                    }));
-                  }}
-                >
-                  <MenuItem value={"1 min"}>1 min</MenuItem>
-                  <MenuItem value={"5 min"}>5 min</MenuItem>
-                  <MenuItem value={"15 min"}>15 min</MenuItem>
-                  <MenuItem value={"1 hour"}>1 hour</MenuItem>
-                  <MenuItem value={"1 day"}>1 day</MenuItem>
-                  <MenuItem value={"all"}>All</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid md={5} sm={12}>
-              <FormControl>
-                <InputLabel sx={{ color: "white" }} id="timerange">
-                  Time range
-                </InputLabel>
-                <Select
-                  sx={{ width: 300 }}
-                  label="Time range"
-                  labelId="timerange"
-                  inputProps={{ sx: { color: "white" } }}
-                  value={settings.timerange}
-                  onChange={(e) => {
-                    setSettings((current) => ({
-                      ...current,
-                      timerange: e.target.value,
-                    }));
-                  }}
-                >
-                  <MenuItem value={0}>London time</MenuItem>
-                  <MenuItem value={1}>Tokyo time</MenuItem>
-                  <MenuItem value={2}>New york time</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid md={5} sm={12}>
-              <FormControl>
-                <InputLabel sx={{ color: "white" }} id="MUD">
-                  Max usage deposite
-                </InputLabel>
-                <Select
-                  sx={{ width: 300 }}
-                  label="Max usage deposite"
-                  labelId="MUD"
-                  inputProps={{ sx: { color: "white" } }}
-                  value={settings.maxUsage}
-                  onChange={(e) => {
-                    setSettings((current) => ({
-                      ...current,
-                      maxUsage: e.target.value,
-                    }));
-                  }}
-                >
-                  <MenuItem value={'35 %'}>35 %</MenuItem>
-                  <MenuItem value={'50 %'}>50 %</MenuItem>
-                  <MenuItem value={'85 %'}>85 %</MenuItem>
-                  <MenuItem value={'100 %'}>100 %</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid md={5} sm={12}>
-              <Button
-                sx={{ color: "white", width: 200, height: 60 }}
-                color="secondary"
-                variant="contained"
-                onClick={handelSubmission}
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </Card>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }
